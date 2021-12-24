f425fd09617982e22fd98c216fb7f3426434fc04
Ludo <ludovic.barusseau@gmail.com>
2021-12-24 10:47:33 +0100

    Initial commit

---
+++ infer_deepsort_process.py
@@ -0,0 +1,269 @@
+# Copyright (C) 2021 Ikomia SAS
+# Contact: https://www.ikomia.com
+#
+# This file is part of the IkomiaStudio software.
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Affero General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU Affero General Public License for more details.
+#
+# You should have received a copy of the GNU Affero General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+from ikomia import core, dataprocess
+import copy
+# Your imports below
+from infer_deepsort.deep_sort_pytorch.utils.parser import get_config
+from infer_deepsort.deep_sort_pytorch.deep_sort import DeepSort
+import torch
+import os
+
+palette = (2 ** 11 - 1, 2 ** 15 - 1, 2 ** 20 - 1)
+
+
+class TrackerDeepSort:
+    def __init__(self):
+        # initialize deepsort
+        self.cfg = get_config()
+        dirname = os.path.dirname(__file__)
+        filename = os.path.join(dirname, 'deep_sort_pytorch/configs/deep_sort.yaml')
+        self.cfg.merge_from_file(filename)
+        self.deepsort = DeepSort(os.path.join(dirname, self.cfg.DEEPSORT.REID_CKPT),
+                                 max_dist=self.cfg.DEEPSORT.MAX_DIST,
+                                 min_confidence=self.cfg.DEEPSORT.MIN_CONFIDENCE,
+                                 nms_max_overlap=self.cfg.DEEPSORT.NMS_MAX_OVERLAP,
+                                 max_iou_distance=self.cfg.DEEPSORT.MAX_IOU_DISTANCE,
+                                 max_age=self.cfg.DEEPSORT.MAX_AGE,
+                                 n_init=self.cfg.DEEPSORT.N_INIT,
+                                 nn_budget=self.cfg.DEEPSORT.NN_BUDGET,
+                                 use_cuda=True)
+
+    @staticmethod
+    def compute_color_for_labels(label):
+        """
+        Simple function that adds fixed color depending on the class
+        """
+        color = [int((p * (label ** 2 - label + 1)) % 255) for p in palette]
+        return color
+
+    def track_deepsort(self, obj_graphics, obj_data, img, param):
+        xywh_boxes = []
+        confidences = []
+        labels = []
+        labels_to_track = param.categories.split(',')
+        self.deepsort.min_confidence = param.confidence
+
+        # Adapt detections to deep sort input format
+        for measure in obj_data:
+            assert(len(measure) >= 2)
+            obj_conf = measure[0]
+            obj_box = measure[1]
+
+            if param.categories == "all" or obj_box.label in labels_to_track:
+                # box to deep sort format
+                box = obj_box.values
+                xywh_boxes.append([box[0]+box[2]/2, box[1]+box[3]/2, box[2], box[3]])
+                confidences.append(obj_conf.values)
+                labels.append(obj_box.label)
+
+        if len(confidences):
+            xywhs = torch.Tensor(xywh_boxes)
+            confs = torch.Tensor(confidences)
+            # pass detections to deepsort
+            outputs, out_confidences, out_labels = self.deepsort.update(xywhs, confs, labels, img)
+        else:
+            self.deepsort.increment_ages()
+            outputs = []
+            out_confidences = []
+            out_labels = []
+
+        return outputs, out_confidences, out_labels
+
+
+# --------------------
+# - Class to handle the process parameters
+# - Inherits PyCore.CWorkflowTaskParam from Ikomia API
+# --------------------
+class DeepSortParam(core.CWorkflowTaskParam):
+
+    def __init__(self):
+        core.CWorkflowTaskParam.__init__(self)
+        # Place default value initialization here
+        self.categories = "all"
+        self.confidence = 0.5
+
+    def setParamMap(self, param_map):
+        # Set parameters values from Ikomia application
+        # Parameters values are stored as string and accessible like a python dict
+        self.categories = param_map["categories"]
+        self.confidence = float(param_map["confidence"])
+
+    def getParamMap(self):
+        # Send parameters values to Ikomia application
+        # Create the specific dict structure (string container)
+        param_map = core.ParamMap()
+        param_map["categories"] = self.categories
+        param_map["confidence"] = str(self.confidence)
+        return param_map
+
+
+# --------------------
+# - Class which implements the process
+# - Inherits PyCore.CWorkflowTask or derived from Ikomia API
+# --------------------
+class DeepSortProcess(dataprocess.C2dImageTask):
+
+    def __init__(self, name, param):
+        dataprocess.C2dImageTask.__init__(self, name)
+        # Add numeric input
+        self.addInput(dataprocess.CBlobMeasureIO())
+        # Add graphics output
+        self.addOutput(dataprocess.CGraphicsOutput())
+        # Add numeric output
+        self.addOutput(dataprocess.CBlobMeasureIO())
+
+        # Create parameters class
+        if param is None:
+            self.setParam(DeepSortParam())
+        else:
+            self.setParam(copy.deepcopy(param))
+
+        self.tracker = TrackerDeepSort()
+
+    def getProgressSteps(self, eltCount=1):
+        # Function returning the number of progress steps for this process
+        # This is handled by the main progress bar of Ikomia application
+        return 2
+
+    def run(self):
+        # Core function of your process
+        # Call beginTaskRun for initialization
+        self.beginTaskRun()
+
+        # Examples :
+        # Get input :
+        img_in = self.getInput(0)
+        graphics_in = self.getInput(1)
+        measures_in = self.getInput(2)
+
+        # Get parameters :
+        param = self.getParam()
+
+        # Get image from input/output (numpy array):
+        src_image = img_in.getImage()
+
+        # Call to the process main routine
+        obj_graphics = graphics_in.getItems()
+        obj_data = measures_in.getMeasures()
+        outputs, confidences, labels = self.tracker.track_deepsort(obj_graphics, obj_data, src_image, param)
+
+        # Step progress bar:
+        self.emitStepProgress()
+
+        # Set image of input/output (numpy array):
+        img_out = self.getOutput(0)
+        img_out.setImage(src_image)
+
+        # Graphics output
+        graphics_out = self.getOutput(1)
+        graphics_out.setNewLayer("DeepSort")
+        graphics_out.setImageIndex(0)
+        # Numeric output
+        numeric_out = self.getOutput(2)
+        numeric_out.clearData()
+
+        if len(outputs) > 0:
+            box_xyxy = outputs[:, :4]
+            identities = outputs[:, -1]
+
+            for i in range(len(identities)):
+                xyxy = box_xyxy[i]
+                x = float(xyxy[0])
+                y = float(xyxy[1])
+                w = float(xyxy[2] - xyxy[0])
+                h = float(xyxy[3] - xyxy[1])
+                track_id = identities[i]
+                color = self.tracker.compute_color_for_labels(track_id)
+
+                # Box
+                prop_rect = core.GraphicsRectProperty()
+                prop_rect.pen_color = color
+                graphics_box = graphics_out.addRectangle(x, y, w, h, prop_rect)
+
+                # Label
+                prop_rect = core.GraphicsRectProperty()
+                color_brush = color
+                color_brush.append(255)
+                prop_rect.pen_color = color_brush
+                prop_rect.brush_color = color_brush
+                graphics_out.addRectangle(x, y - 20, 20, 20, prop_rect)
+                prop_text = core.GraphicsTextProperty()
+                prop_text.font_size = 8
+                prop_text.color = color
+                prop_text.bold = True
+                graphics_out.addText(str(track_id), x, y, prop_text)
+
+                # object results
+                results = []
+                track_data = dataprocess.CObjectMeasure(dataprocess.CMeasure(core.MeasureId.CUSTOM, "Track ID"),
+                                                        float(track_id),
+                                                        graphics_box.getId(),
+                                                        labels[i])
+                confidence_data = dataprocess.CObjectMeasure(dataprocess.CMeasure(core.MeasureId.CUSTOM, "Confidence"),
+                                                             confidences[i],
+                                                             graphics_box.getId(),
+                                                             labels[i])
+                box_data = dataprocess.CObjectMeasure(dataprocess.CMeasure(core.MeasureId.BBOX),
+                                                      [x, y, w, h],
+                                                      graphics_box.getId(),
+                                                      labels[i])
+                results.append(track_data)
+                results.append(confidence_data)
+                results.append(box_data)
+                numeric_out.addObjectMeasures(results)
+
+        # Step progress bar:
+        self.emitStepProgress()
+
+        # Call endTaskRun to finalize process
+        self.endTaskRun()
+
+
+# --------------------
+# - Factory class to build process object
+# - Inherits PyDataProcess.CTaskFactory from Ikomia API
+# --------------------
+class DeepSortProcessFactory(dataprocess.CTaskFactory):
+
+    def __init__(self):
+        dataprocess.CTaskFactory.__init__(self)
+        # Set process information as string here
+        self.info.name = "infer_deepsort"
+        self.info.shortDescription = "your short description"
+        self.info.description = "your description"
+        self.info.authors = "Plugin authors"
+        # relative path -> as displayed in Ikomia application process tree
+        self.info.path = "Plugins/Python"
+        self.info.version = "1.0.0"
+        # self.info.iconPath = "your path to a specific icon"
+        self.info.authors = "algorithm author"
+        self.info.article = "title of associated research article"
+        self.info.journal = "publication journal"
+        self.info.year = 2021
+        self.info.license = "MIT License"
+        # URL of documentation
+        self.info.documentationLink = ""
+        # Code source repository
+        self.info.repository = ""
+        # Keywords used for search
+        self.info.keywords = "your,keywords,here"
+
+    def create(self, param=None):
+        # Create process object
+        return DeepSortProcess(self.info.name, param)
---
+++ infer_deepsort_widget.py
@@ -0,0 +1,76 @@
+# Copyright (C) 2021 Ikomia SAS
+# Contact: https://www.ikomia.com
+#
+# This file is part of the IkomiaStudio software.
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Affero General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU Affero General Public License for more details.
+#
+# You should have received a copy of the GNU Affero General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+from ikomia import core, dataprocess
+from ikomia.utils import pyqtutils, qtconversion
+from infer_deepsort.infer_deepsort_process import DeepSortParam
+# PyQt GUI framework
+from PyQt5.QtWidgets import *
+
+
+# --------------------
+# - Class which implements widget associated with the process
+# - Inherits PyCore.CWorkflowTaskWidget from Ikomia API
+# --------------------
+class DeepSortWidget(core.CWorkflowTaskWidget):
+
+    def __init__(self, param, parent):
+        core.CWorkflowTaskWidget.__init__(self, parent)
+
+        if param is None:
+            self.parameters = DeepSortParam()
+        else:
+            self.parameters = param
+
+        # Create layout : QGridLayout by default
+        self.grid_layout = QGridLayout()
+
+        self.spin_conf = pyqtutils.append_double_spin(self.grid_layout, "Confidence min",
+                                                      self.parameters.confidence, 0.0, 1.0, 0.1, 2)
+
+        self.edit_categories = pyqtutils.append_edit(self.grid_layout, "Categories", self.parameters.categories)
+
+        # PyQt -> Qt wrapping
+        layout_ptr = qtconversion.PyQtToQt(self.grid_layout)
+        # Set widget layout
+        self.setLayout(layout_ptr)
+
+    def onApply(self):
+        # Apply button clicked slot
+        # Get parameters from widget
+        self.parameters.confidence = self.spin_conf.value()
+        self.parameters.categories = self.edit_categories.text()
+
+        # Send signal to launch the process
+        self.emitApply(self.parameters)
+
+
+# --------------------
+# - Factory class to build process widget object
+# - Inherits PyDataProcess.CWidgetFactory from Ikomia API
+# --------------------
+class DeepSortWidgetFactory(dataprocess.CWidgetFactory):
+
+    def __init__(self):
+        dataprocess.CWidgetFactory.__init__(self)
+        # Set the name of the process -> it must be the same as the one declared in the process factory class
+        self.name = "infer_deepsort"
+
+    def create(self, param):
+        # Create widget object
+        return DeepSortWidget(param, None)
