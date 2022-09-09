992099bc77ff4f37009036d98655c2e3f50b620f
Ludo <ludovic.barusseau@gmail.com>
2022-09-09 12:25:05 +0200

    Use CObjectDetectionIO input type

--- infer_deepsort_process.py
+++ infer_deepsort_process.py
@@ -52,7 +52,7 @@ class TrackerDeepSort:
         color = [int((p * (label ** 2 - label + 1)) % 255) for p in palette]
         return color
 
-    def track_deepsort(self, obj_data, img, param):
+    def track_deepsort(self, obj_detect_in, img, param):
         xywh_boxes = []
         confidences = []
         labels = []
@@ -60,17 +60,12 @@ class TrackerDeepSort:
         self.deepsort.min_confidence = param.confidence
 
         # Adapt detections to deep sort input format
-        for measure in obj_data:
-            assert(len(measure) >= 2)
-            obj_conf = measure[0]
-            obj_box = measure[1]
-
-            if param.categories == "all" or obj_box.label in labels_to_track:
+        for obj in obj_detect_in.getObjects():
+            if param.categories == "all" or obj.label in labels_to_track:
                 # box to deep sort format
-                box = obj_box.values
-                xywh_boxes.append([box[0]+box[2]/2, box[1]+box[3]/2, box[2], box[3]])
-                confidences.append(obj_conf.values)
-                labels.append(obj_box.label)
+                xywh_boxes.append([obj.box[0]+obj.box[2]/2, obj.box[1]+obj.box[3]/2, obj.box[2], obj.box[3]])
+                confidences.append(obj.confidence)
+                labels.append(obj.label)
 
         if len(confidences):
             xywhs = torch.Tensor(xywh_boxes)
@@ -121,12 +116,11 @@ class DeepSortProcess(dataprocess.C2dImageTask):
 
     def __init__(self, name, param):
         dataprocess.C2dImageTask.__init__(self, name)
-        # Add numeric input
-        self.addInput(dataprocess.CBlobMeasureIO())
-        # Add graphics output
-        self.addOutput(dataprocess.CGraphicsOutput())
-        # Add numeric output
-        self.addOutput(dataprocess.CBlobMeasureIO())
+        self.removeInput(1)
+        # Add object detection input
+        self.addInput(dataprocess.CObjectDetectionIO())
+        # Add object detection output
+        self.addOutput(dataprocess.CObjectDetectionIO())
 
         # Create parameters class
         if param is None:
@@ -149,7 +143,7 @@ class DeepSortProcess(dataprocess.C2dImageTask):
         # Examples :
         # Get input :
         img_in = self.getInput(0)
-        measures_in = self.getInput(2)
+        obj_detect_in = self.getInput(1)
 
         # Get parameters :
         param = self.getParam()
@@ -158,8 +152,7 @@ class DeepSortProcess(dataprocess.C2dImageTask):
         src_image = img_in.getImage()
 
         # Call to the process main routine
-        obj_data = measures_in.getMeasures()
-        outputs, confidences, labels = self.tracker.track_deepsort(obj_data, src_image, param)
+        outputs, confidences, labels = self.tracker.track_deepsort(obj_detect_in, src_image, param)
 
         # Step progress bar:
         self.emitStepProgress()
@@ -168,13 +161,9 @@ class DeepSortProcess(dataprocess.C2dImageTask):
         img_out = self.getOutput(0)
         img_out.setImage(src_image)
 
-        # Graphics output
-        graphics_out = self.getOutput(1)
-        graphics_out.setNewLayer("DeepSort")
-        graphics_out.setImageIndex(0)
-        # Numeric output
-        numeric_out = self.getOutput(2)
-        numeric_out.clearData()
+        # Object detection output
+        obj_detect_out = self.getOutput(1)
+        obj_detect_out.init("DeepSort", 0)
 
         if len(outputs) > 0:
             box_xyxy = outputs[:, :4]
@@ -188,44 +177,7 @@ class DeepSortProcess(dataprocess.C2dImageTask):
                 h = float(xyxy[3] - xyxy[1])
                 track_id = identities[i]
                 color = self.tracker.compute_color_for_labels(track_id)
-
-                # Box
-                prop_rect = core.GraphicsRectProperty()
-                prop_rect.pen_color = color
-                graphics_box = graphics_out.addRectangle(x, y, w, h, prop_rect)
-
-                # Label
-                prop_rect = core.GraphicsRectProperty()
-                color_brush = color
-                color_brush.append(255)
-                prop_rect.pen_color = color_brush
-                prop_rect.brush_color = color_brush
-                graphics_out.addRectangle(x, y - 20, 20, 20, prop_rect)
-                prop_text = core.GraphicsTextProperty()
-                prop_text.font_size = 8
-                prop_text.color = color
-                prop_text.bold = True
-                label = f"{track_id}:{labels[i]}"
-                graphics_out.addText(label, x, y, prop_text)
-
-                # object results
-                results = []
-                track_data = dataprocess.CObjectMeasure(dataprocess.CMeasure(core.MeasureId.CUSTOM, "Track ID"),
-                                                        float(track_id),
-                                                        graphics_box.getId(),
-                                                        labels[i])
-                confidence_data = dataprocess.CObjectMeasure(dataprocess.CMeasure(core.MeasureId.CUSTOM, "Confidence"),
-                                                             confidences[i],
-                                                             graphics_box.getId(),
-                                                             labels[i])
-                box_data = dataprocess.CObjectMeasure(dataprocess.CMeasure(core.MeasureId.BBOX),
-                                                      [x, y, w, h],
-                                                      graphics_box.getId(),
-                                                      labels[i])
-                results.append(track_data)
-                results.append(confidence_data)
-                results.append(box_data)
-                numeric_out.addObjectMeasures(results)
+                obj_detect_out.addObject(int(track_id), labels[i], confidences[i], x, y, w, h, color)
 
         # Step progress bar:
         self.emitStepProgress()
